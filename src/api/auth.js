40f3a0ffc1eda8a501034f40e36019ec95aeb794
Rahulrs7 <Rrahul5223u@gmail.com>
2025-03-08 10:44:03 +0530

    url update

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api/admin'; // Update this if needed
+const API_URL = 'https://quickdelbackend.onrender.com/api/admin'; // Update this if needed
 
 export const registerAdmin = async (adminData) => {
     return await axios.post(`${API_URL}/register`, adminData);
